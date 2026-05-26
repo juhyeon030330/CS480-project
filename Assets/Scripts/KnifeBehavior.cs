456a2d73c90c3385e2288b2896b3a7a4a0ee5615
Lachlan Domst <lachlandomst@wless-10-108-254-37.wless.uoregon.edu>
2026-05-26 02:15:48 -0700

    Moved some things to the player prefab for consistency's sake and added stick ability only when hitting an enemy and a pickup ability. Damage currently doesn't work properly

--- Assets/Scripts/KnifeBehavior.cs
+++ Assets/Scripts/KnifeBehavior.cs
@@ -3,25 +3,70 @@ using UnityEngine;
 public class KnifeBehavior : MonoBehaviour
 {
     private Rigidbody RB;
-
-    private bool targetHit;
+    
+    private bool targetHit = false;
 
     private void Start()
     {
         RB = GetComponent<Rigidbody>();
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision other)
     {
-        if (targetHit)
-            return;
-        else
+        // Determine if player was touched, in which case it is picked up
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.GetComponent<PlayerThrowingKnife>().totalKnives++;
+
+            // Deal more damage as it is picked up if lodged in an enemy
+            if (targetHit == true) {
+                // ===== DAMAGE ===== (copied from FirstPersonController)
+                // Build the damage package
+                DamageData knifePickupDmg = new DamageData
+                {
+                    amount = 10f,
+                    source = this.gameObject,
+                    hitDirection = (other.gameObject.transform.position - transform.position).normalized
+                };
+
+                // Deal damage
+                if (other.gameObject.TryGetComponent<IDamageable>(out var target))
+                {
+                    target.TakeDamage(knifePickupDmg);
+                }
+            }
+
+            Destroy(gameObject);
+        }
+        // Determine if a target was hit
+        if (other.gameObject.CompareTag("Enemy") || other.gameObject.CompareTag("Boss"))
+        {
             targetHit = true;
+        }
+        else {
+            targetHit = false;
+        }
 
-        RB.isKinematic = true;
+        if (targetHit == true)
+        {
+            // ===== DAMAGE ===== (copied from FirstPersonController)
+            // Build the damage package
+            DamageData knifeDmg = new DamageData
+            {
+                amount = 25f,
+                source = this.gameObject,
+                hitDirection = (other.gameObject.transform.position - transform.position).normalized
+            };
 
-        transform.SetParent(collision.transform);
+            // Deal damage
+            if (other.gameObject.TryGetComponent<IDamageable>(out var target))
+            {
+                target.TakeDamage(knifeDmg);
+            }
+
+            // Stick to enemy
+            RB.isKinematic = true;
+            transform.SetParent(other.gameObject.transform);
+        }
     }
-
-
 }
